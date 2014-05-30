a30f8fd874a9c21b2600a701d9f2bc3048c14885
Mike Collins <mdcollins@ebay.com>
2014-05-29 18:16:52 -0700

    iterating

---
+++ a.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+	
+print "Content-type:text/html\r\n\r\n";
+print "<html>";
+print "<head>";
+print "<title>Received POST</title>";
+print "</head>";
+print "<body>";
+print ": <br>";
+print "</body>";
+print "</html>";
+
+1;
+
