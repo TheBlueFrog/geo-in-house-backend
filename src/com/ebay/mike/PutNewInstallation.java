48d3514004f48c22b7738c827ca7535d77eeac78
Collins, Mike <mdcollins@ebay.com>
2014-06-18 19:20:44 -0700

    setup for pushing the install guid into the C3 database

--- src/com/ebay/mike/PutNewInstallation.java
+++ src/com/ebay/mike/PutNewInstallation.java
@@ -3,6 +3,8 @@ package com.ebay.mike;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.ebay.mike.geodb.InstallationRecord;
 
@@ -15,16 +17,25 @@ public class PutNewInstallation
 	 * app does a POST to http://66.211.190.18/cgi-bin/toJava.pl
 	 * 
 	 * if OpCode = PutNewInstallation we get here
-	 * we return a new installation record
+	 * we return the toString of the new installation record
 
-	 * args
-	 * [0] = new installation GUID
-	 * [1] = display name
-	 * [2] = new GCM registration ID 
+	 * args are a map
+	 * 
+	 *  params.put("InstallationGUID", installGuid);
+		params.put("DisplayName", displayName);
+		params.put("GCMRegistrationID", gcmRegistrationID);
 	 */
 	
 	public static void main(String[] args)
 	{
+		Map<String, String> params = new HashMap<String, String>();
+		
+		for (int i = 0; i < args.length; i += 2)
+		{
+			params.put(args[i], args[i+1]);
+			System.out.println(String.format("%s = %s", args[i], args[i+1]));
+		}
+
 	    try
 	    {
 			Class.forName("org.sqlite.JDBC");
@@ -35,7 +46,10 @@ public class PutNewInstallation
 		    {
 		      db = DriverManager.getConnection("jdbc:sqlite:fencenotification.db");
 
-		      InstallationRecord r = new InstallationRecord(db, args[0], args[1], args[2]);
+		      InstallationRecord r = new InstallationRecord(db, 
+		    		  params.get("InstallationGUID"), 
+		    		  params.get("DisplayName"), 
+		    		  params.get("GCMRegistrationID"));
 		      
 		      System.out.println(r.toString());
 		    }
